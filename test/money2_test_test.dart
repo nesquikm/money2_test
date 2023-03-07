5588917bb109b620dc843043b4a1782565d09712
nesquikm <drapolyuk@gmail.com>
2023-03-07 15:32:14 +0400

    fix: move BigInt test

--- test/money2_test_test.dart
+++ test/money2_test_test.dart
@@ -45,12 +45,4 @@ void main() {
       }
     }
   });
-
-  test('integers BigInt test', () {
-    for (var ints = 0; ints <= 100; ints++) {
-      final intsStr = ints == 0 ? '0' : '9' * ints;
-      final bi = BigInt.parse(intsStr);
-      expect(bi.toString(), intsStr, reason: '$ints ints');
-    }
-  });
 }
